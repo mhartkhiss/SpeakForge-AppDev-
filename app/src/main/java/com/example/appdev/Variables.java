4ca452f89b4ed74302d8b20eafdfa89244edccd2
Makizz <mhartkhiss.degollacion@cit.edu>
2025-04-16 14:11:30 +0800

    Update Gradle and API configurations
    - Upgrade Android Gradle plugin version from 8.8.2 to 8.9.1 for improved build performance and features.
    - Update Gradle wrapper to version 8.11.1 for compatibility with the latest features and enhancements.

--- app/src/main/java/com/example/appdev/Variables.java
+++ app/src/main/java/com/example/appdev/Variables.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Variables {
     // Constant variables
-    public static final String API_BASE_URL = "https://speakforge-a52586b8a3f8.herokuapp.com/api/";
+    public static final String API_BASE_URL = "https://n7mgqpz5-8000.asse.devtunnels.ms/api/";
     public static final String API_TRANSLATE_DB_URL = API_BASE_URL + "translate-db/";
     public static final String API_TRANSLATE_GROUP_URL = API_BASE_URL + "translate-group/";
     public static final String API_TRANSLATE_GROUP_CONTEXT_URL = API_BASE_URL + "translate-group-context/";
